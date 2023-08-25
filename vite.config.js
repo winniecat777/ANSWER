e209551e9817f6f0111955b6d1f498c1e7ed7e6a
winniecat777 <winniecat777@gmail.com>
2023-08-25 17:44:19 +0800

    fix vite

--- vite.config.js
+++ vite.config.js
@@ -33,7 +33,8 @@ export default defineConfig({
   ],
   server: {
     // 啟動 server 時預設開啟的頁面
-    open: 'pages/index.html',
+    //open: 'pages/index.html',
+    open: 'pages/assignment.html',
   },
   build: {
     rollupOptions: {
